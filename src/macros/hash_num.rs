96301a85300aefc22cc7c39f11507218ad0fbff1
Hal Henke <halhenke@gmail.com>
2018-06-08 03:08:16 +1000

    Got my macro working

--- src/macros/hash_num.rs
+++ src/macros/hash_num.rs
@@ -15,73 +15,54 @@ macro_rules! mkHash {
         )
 }
 
-pub fn invert_map<K, V, S>(hmap: HashMap<K, V, S>) -> HashMap<V, K, S> where
+// trait Invertible<K, V>
+//     // where V: Hash + Eq
+//     // where V: std::cmp::Hash,
+// {
+//     type H;
+//     fn invert(&self) -> Self::H;
+//     // fn invert(& self) -> Self;
+// }
+
+/// A trait that lets you invert a HashMap so Keys become Values & vice versa
+trait Invertible<K, V, S> where
     V: Hash + Eq,
     K: Eq + Hash,
     S: BuildHasher + Default,
 {
-    hmap.into_iter()
-        .map(|(k,v)| (v,k) )
-        // .collect::<HashMap<V, K, S>>()
-        .collect()
+    type H;
+    fn invert(self) -> Self::H;
+}
+
+impl<K, V, S> Invertible<K, V, S> for HashMap<K, V, S> where
+    V: Hash + Eq,
+    K: Eq + Hash,
+    S: BuildHasher + Default,
+{
+    type H = HashMap<V, K, S>;
+
+    // fn invert<K, V, S>(hmap: HashMap<K, V, S>) -> HashMap<V, K, S> where
+    fn invert(self) -> HashMap<V, K, S> where
+    V: Hash + Eq,
+    K: Eq + Hash,
+    S: BuildHasher + Default,
+    {
+        self.into_iter()
+            .map(|(k,v)| (v,k) )
+            // .collect::<HashMap<V, K, S>>()
+            .collect()
+    }
 }
 
 
-// trait Invertible<K, V>
-    // where V: Hash + Eq
-//     // where V: std::cmp::Hash,
+// Original working version of invert
+// pub fn invert_map<K, V, S>(hmap: HashMap<K, V, S>) -> HashMap<V, K, S> where
+//     V: Hash + Eq,
+//     K: Eq + Hash,
+//     S: BuildHasher + Default,
 // {
-//     type H;
-//     fn invert(& self) -> Self::H;
-//     // fn invert(& self) -> Self;
-// }
-
-
-
-// // impl<K, V> Invertible<K, V> for HashMap<K, V> {
-// //     fn invert(&self) -> HashMap<V, K> {
-// //     // fn invert(&self) -> HashMap<K, V> {
-// //         unimplemented!();
-// //     }
-// // }
-
-// impl<K, V> Invertible<K, V> for HashMap<K, V>
-//     where   V: Hash + Eq,
-//             K: Hash + Eq
-// {
-//     type H = HashMap<V, K>;
-
-//     // fn invert(&self) -> HashMap<K, V> {
-//     fn invert(&self) -> Self::H {
-//         let mut h_new: Self::H = HashMap::new();
-//         // let kvs = self.iter()
-//         //     .collect::<Vec<(&K, &V)>>();
-
-//         self.into_iter()
-//             .map(||)
-
-//         // let tlist: Vec<(&V, &K)> = self.into_iter()
-//         //     .map(|(k, v)| (v, k))
-//         //     .collect();
-//         // let tlist = self.into_iter()
-//         //     // .cloned()
-//         //     // .map(|(k, v)| (v, k))
-//         //     .collect();
-//             // .iter();
-
-//             // .for_each(move |(k, v)| println!("Hey"))
-//             // .map(move |(k, v)| (k, v))
-//             // .cloned()
-//             // .for_each(
-//             //     |(&k, &v)| {
-//             //         h_new.insert(v, k).unwrap();
-//             //         return ()
-//             //         }
-//             // );
-//         // for (k, v) in self.iter() {
-//         //     h_new.insert(v, k).unwrap();
-//         // }
-//         // HashMap::from_iter(tlist)
-//         tlist
-//     }
+//     hmap.into_iter()
+//         .map(|(k,v)| (v,k) )
+//         // .collect::<HashMap<V, K, S>>()
+//         .collect()
 // }
