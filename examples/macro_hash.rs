21175148b29bc9e694a11f81376d37cf37fc3af6
Hal Henke <halhenke@gmail.com>
2018-06-08 03:34:26 +1000

    Got my invertible macro working

---
+++ examples/macro_hash.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate git_global;
+use git_global::macros::hash_num::*;
+
+fn main() ->() {
+    mkHash!(
+        the_thing,
+        "a1",
+        "a2",
+        "a3"
+    );
+    println!("Macro Hash");
+
+    println!("{}", the_thing.get("a2").unwrap());
+    println!("{}", the_thing.invert().get(&1).unwrap());
+}
--- src/macros/hash_num.rs
+++ src/macros/hash_num.rs
@@ -1,18 +1,51 @@
 use std::collections::HashMap;
 // use std::prelude::v1
 // use std::hash::Hash;
-use std::hash::{BuildHasher, Hash};
-use std::cmp::Eq;
 // use std::iter::FromIterator;
 
 #[macro_export]
 macro_rules! mkHash {
-    ( $e:item, $( $s:item ),* ) => (
+    ( $e:ident, $( $s:expr ),* ) => (
+
+        use std::collections::HashMap;
+        use std::hash::{BuildHasher, Hash};
+        use std::cmp::Eq;
+
         let mut $e = HashMap::new()
         let mut i: i32 = 0;
         $($e.insert($s, i);
         i = i + 1;)*
-        )
+
+        /// A trait that lets you invert a HashMap so Keys become Values & vice versa
+        trait Invertible<K, V, S> where
+            V: Hash + Eq,
+            K: Eq + Hash,
+            S: BuildHasher + Default,
+        {
+            type H;
+            fn invert(self) -> Self::H;
+        }
+
+        impl<K, V, S> Invertible<K, V, S> for HashMap<K, V, S> where
+            V: Hash + Eq,
+            K: Eq + Hash,
+            S: BuildHasher + Default,
+        {
+            type H = HashMap<V, K, S>;
+
+            // fn invert<K, V, S>(hmap: HashMap<K, V, S>) -> HashMap<V, K, S> where
+            fn invert(self) -> HashMap<V, K, S> where
+            V: Hash + Eq,
+            K: Eq + Hash,
+            S: BuildHasher + Default,
+            {
+                self.into_iter()
+                    .map(|(k,v)| (v,k) )
+                    // .collect::<HashMap<V, K, S>>()
+                    .collect()
+            }
+        }
+    )
 }
 
 // trait Invertible<K, V>
@@ -24,35 +57,7 @@ macro_rules! mkHash {
 //     // fn invert(& self) -> Self;
 // }
 
-/// A trait that lets you invert a HashMap so Keys become Values & vice versa
-trait Invertible<K, V, S> where
-    V: Hash + Eq,
-    K: Eq + Hash,
-    S: BuildHasher + Default,
-{
-    type H;
-    fn invert(self) -> Self::H;
-}
 
-impl<K, V, S> Invertible<K, V, S> for HashMap<K, V, S> where
-    V: Hash + Eq,
-    K: Eq + Hash,
-    S: BuildHasher + Default,
-{
-    type H = HashMap<V, K, S>;
-
-    // fn invert<K, V, S>(hmap: HashMap<K, V, S>) -> HashMap<V, K, S> where
-    fn invert(self) -> HashMap<V, K, S> where
-    V: Hash + Eq,
-    K: Eq + Hash,
-    S: BuildHasher + Default,
-    {
-        self.into_iter()
-            .map(|(k,v)| (v,k) )
-            // .collect::<HashMap<V, K, S>>()
-            .collect()
-    }
-}
 
 
 // Original working version of invert
