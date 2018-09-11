216517da72111b56ee806545fa59dc3f17205fc5
Hal Henke <halhenke@gmail.com>
2018-09-12 06:13:09 +1000

    Me trying to use a build script to generate Shell completions all the time
    - Didnt work because the `include!` macro doesnt recognise other imports

---
+++ build.rs
@@ -0,0 +1,20 @@
+extern crate clap;
+use clap::Shell;
+
+// include!("src/lib.rs");
+// include!("src/cli.rs");
+
+// use std::env;
+
+fn main() {
+    println!("Too hard at present to use this build script to generate completions...");
+
+    // let outdir = match env::var_os("OUT_DIR") {
+    //     None => return,
+    //     Some(outdir) => outdir,
+    // };
+    // let mut app = get_clap_app();
+    // app.gen_completions("myapp",      // We need to specify the bin name manually
+    //                     Shell::Bash,  // Then say which shell to build completions for
+    //                     outdir);      // Then say where write the completions to
+}
