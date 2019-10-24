2dddb793e381a7dcaada3a816bd4265d2fb5e769
Hal Henke <halhenke@gmail.com>
2019-10-25 02:09:58 +1100

    Make the channel bounded & increase to 8 threads - much faster 4.5 seconds
    - clean up

--- src/subcommands/new_status.rs
+++ src/subcommands/new_status.rs
@@ -16,9 +16,7 @@ use repo::{get_repos, GitGlobalError, GitGlobalResult, Repo};
 pub fn get_results(
     only_modified: bool,
     ignore_untracked: bool,
-    // path_filter: Option<&str>,
     path_filter: Option<String>,
-    // path_filter: Option<&'static str>,
 ) -> Result<GitGlobalResult> {
     let include_untracked = true;
     // let include_untracked = config.show_untracked;
@@ -26,18 +24,11 @@ pub fn get_results(
     let n_repos = repos.len();
     let mut result = GitGlobalResult::new(&repos);
     result.pad_repo_output();
-    // let mut result =
-    //     Arc::new(Mutex::new(GitGlobalResult::new(&repos).pad_repo_output()));
 
-    // let (s, r) = bounded(2);
-    let (s, r) = unbounded();
+    let (s, r) = bounded(10);
+    // let (s, r) = unbounded();
 
     // TODO: limit number of threads, perhaps with mpsc::sync_channel(n)?
-    // let (tx, rx): (
-    //     std::sync::mpsc::Sender<(String, Vec<String>)>,
-    //     std::sync::mpsc::Receiver<(_, _)>,
-    // ) = mpsc::channel();
-    // let (tx, rx) = mpsc::channel();
 
     for repo in repos {
         let s = s.clone();
@@ -60,44 +51,28 @@ pub fn get_results(
             s.send((path, lines)).unwrap();
         });
     }
-    // r.iter().for_each(|(path, lines)| {
-    // });
     type ArMuGgr = Arc<Mutex<GitGlobalResult>>;
     let mut resvec: Vec<ArMuGgr> = vec![];
-    // let mut resvec: Vec<GitGlobalResult> = vec![];
     let pf = Arc::new(path_filter);
     let result: Arc<Mutex<GitGlobalResult>> = Arc::new(Mutex::new(result));
 
-    for _ in 0..2 {
-        // let r = Arc::clone(&r);
-        // let pf = Arc::clone(&pf);
-        // let result = Arc::clone(&result);
+    let thread_count = 8;
+    for _ in 0..thread_count {
         let r = r.clone();
         let pf = pf.clone();
         let result = result.clone();
 
-        // let pf: &'static str = path_filter.unwrap().clone();
-        // println!("i is {}", i);
         let j = thread::spawn(move || {
-            for _ in 0..(n_repos / 2) {
+            for _ in 0..(n_repos / thread_count) {
                 let out = r.recv().unwrap();
                 let (path, lines): (String, Vec<String>) = out;
-                // let (path: String, lines: Vec<String>) = r.recv().unwrap();
-
-                // debug!("We are in {}\n", path);
 
                 if let Some(pf) = &(*pf) {
                     if !path.contains(pf) {
                         continue;
-                        // return;
                     }
                 }
-                // let mut result = result.lock().unwrap();
                 let mut result = result.lock().unwrap();
-                // let result: &mut GitGlobalResult =
-                //     Arc::get_mut(&mut result).unwrap();
-                // let pre_res = result.get_mut();
-                // let result: &mut GitGlobalResult = (result.get_mut()).unwrap();
 
                 let repo = Repo::new(path.to_string());
 
@@ -120,32 +95,11 @@ pub fn get_results(
             }
             return result;
         });
-        // let ac: Arc<Mutex<GitGlobalResult>> =
-        //     j.join().expect("Arc unwrap failure!");
         let ac: Arc<Mutex<GitGlobalResult>> =
             j.join().expect("Arc unwrap failure!");
-        // let acb: GitGlobalResult = Arc::try_unwrap(ac)
-        //     .expect("preCommand failed")
-        //     .into_inner()
-        //     .expect("Mutex unwrap failure!");
-        // resvec.push(ac);
-        // resvec.push(acb);
-        // resvec.push(j.join().unwrap().into_inner().unwrap().clone());
-        // vec![]
     }
-
-    // let ac: Arc<Mutex<GitGlobalResult>> =
-    //     j.join().expect("Arc unwrap failure!");
-    // let acb: GitGlobalResult = Arc::try_unwrap(resvec.remove(0))
-    //     .expect("preCommand failed")
-    //     .into_inner()
-    //     .expect("Mutex unwrap failure!");
-
-    // Err(GitGlobalError::BadSubcommand("whoops".to_string()))
     Ok(Arc::try_unwrap(result)
         .expect("preCommand failed")
         .into_inner()
         .expect("Mutex unwrap failure!"))
-    // Ok(resvec.remove(0))
-    // Ok((*result).into_inner().unwrap())
 }
