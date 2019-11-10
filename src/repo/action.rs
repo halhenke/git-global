3a501b40dd20e4afd00153092f1357b81f17088e
Hal Henke <halhenke@gmail.com>
2019-11-11 03:36:10 +1100

    Bunch of helper functions to perform an action

--- src/repo/action.rs
+++ src/repo/action.rs
@@ -50,9 +50,11 @@ pub enum Action {
     NonPathAction(CommandName, Command, Vec<String>),
 }
 
+#[derive(Clone, Debug)]
 pub enum ActionError {
     ActionFailed,
     NeedAPath(String),
+    NotANeedAPath(String),
 }
 
 type ActionResult<T> = Result<T, ActionError>;
@@ -62,29 +64,152 @@ impl Display for Action {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), Error> {
         // return String::from("Booyah");
         // *f = String::from("Booyah");
-        f.write_str("ooyah");
+        // f.write_str(self.);
+        match self {
+            // Action::GitAction(path, name, _, _) => {},
+            Action::PathAction(path, name, _, _) => {
+                f.write_str(&format!("Action `{}` for path {}", name, path));
+            }
+            Action::NeedsAPathAction(name, _, _) => {
+                f.write_str(&format!(
+                    "Action `{}` needs to be associated with a path before execution",
+                    name
+                ));
+            }
+            Action::NonPathAction(name, _, _) => {
+                f.write_str(&format!(
+                    "Action `{}` not associated with a path",
+                    name
+                ));
+            }
+        }
         Ok(())
     }
 }
 
 impl Action {
+    /// Check if
+    /// - this is a [`NeedsAPathAction`] variant
+    /// - the command name matches
+    /// If yes, bind the path and return
+    /// Method calls [`name_match`] and [`path_bind`]
+    pub fn check_needs_path_match_name(
+        &self,
+        action: &str,
+        path: String,
+    ) -> Option<Self> {
+        if self.name_match(action).is_none() {
+            return None;
+        }
+        // if let Ok(bound_action) = self.path_bind(path) {
+        //     return Some(bound_action);
+        // }
+        match self.path_bind(path) {
+            Ok(bound_action) => Some(bound_action),
+            Err(_) => None,
+        }
+    }
+
+    /// return Action name
+    pub fn get_name(&self) -> &str {
+        match self {
+            Action::PathAction(_, name, ..) => name,
+            Action::NeedsAPathAction(name, ..)
+            | Action::NonPathAction(name, ..) => name,
+        }
+    }
+
+    /// Indicates whether the action needs to be provided with a path in which to be executed - i.e. is it a [`Action::NeedsAPathAction`]
+    pub fn needs_path(&self) -> bool {
+        match self {
+            Action::NeedsAPathAction(..) => true,
+            _ => false,
+        }
+    }
+
+    /// Check if a given string matches the name of the action
+    pub fn name_match(&self, act: &str) -> Option<Self> {
+        match self {
+            Action::PathAction(path, name, cmd, args) => {
+                if name == act {
+                    return Some(self.clone());
+                }
+            }
+            Action::NeedsAPathAction(name, _, _) => {
+                if name == &act {
+                    return Some(self.clone());
+                }
+            }
+            Action::NonPathAction(name, _, _) => {
+                if name == &act {
+                    return Some(self.clone());
+                }
+            }
+        }
+        return None;
+        // return Some(self.clone());
+    }
+
+    /// Turn a [`NeedsAPathAction`] variant into a [`PathAction`]
+    pub fn path_bind(&self, path: String) -> Result<Self, ActionError> {
+        match self {
+            Action::NeedsAPathAction(name, cmd, args) => {
+                Ok(Action::PathAction(
+                    path,
+                    name.to_owned(),
+                    cmd.to_owned(),
+                    args.to_owned(),
+                ))
+            }
+            // _ => Err(ActionError::NotANeedAPath(format!(
+            // Action::NonPathAction(name, cmd, args) => {
+            //     Err(ActionError::NotANeedAPath(format!(
+            //         "Action {} is not a Action::NeedsAPathAction",
+            //         "self.name"
+            //     )))
+            // }
+            Action::NonPathAction(name, cmd, args)
+            | Action::PathAction(_, name, cmd, args) => {
+                Err(ActionError::NotANeedAPath(format!(
+                    "Action {} is not a Action::NeedsAPathAction",
+                    name
+                )))
+            }
+        }
+    }
+
+    /// If we have a [`Action::PathAction`] or a [`Action::NonPathAction`] perform the action and return output as a string.
+    /// If we have a [`Action::NeedsAPathAction`] then return an Error
     pub fn perform_action_for_repo(&self) -> ActionResult<(String)> {
         match self {
-            Action::GitAction(path, name, cmd, args) => {
+            Action::PathAction(path, name, cmd, args) => {
+                let r = Exec::shell(cmd)
+                    .args(args)
+                    .cwd(path)
+                    .capture()
+                    .unwrap()
+                    .stdout_str();
+                // println!("Here is some r: {}", r);
+                Ok(r)
+            }
+            Action::NonPathAction(name, cmd, args) => {
                 let r =
-                    Exec::shell(cmd).args(args).cwd(path).capture().unwrap().stdout_str();
+                    Exec::shell(cmd).args(args).capture().unwrap().stdout_str();
                 Ok(r)
-
-                // Ok("Yo")
             }
-            Action::PathAction(name, _, _) => Err(ActionError::NeedAPath(format!("The command {} needs a path to be performed in", name).to_owned())),
-            Action::NonGitAction(name, cmd, args) => {
-                let r = Exec::shell(cmd).args(args).capture().unwrap().stdout_str();
-                Ok(r)
-                // Ok("No".to_owned())
-            }
-            // GitAction => println!("Yo"),
-            // NonGitAction => println!("No"),
+            Action::NeedsAPathAction(name, _, _) => {
+                Err(ActionError::NeedAPath(
+                    format!(
+                        "The command {} needs a path to be performed in",
+                        name
+                    )
+                    .to_owned(),
+                ))
+            } // Action::NonGitAction(name, cmd, args) => {
+              //     let r = Exec::shell(cmd).args(args).capture().unwrap().stdout_str();
+              //     Ok(r)
+              //     // Ok("No".to_owned())
+              // }
         }
         // return format!("Performing action {} for Repo {}", self.0, repo.path);
         // // println!("Performing action {} for Repo :{}", self.0, repo.path);
--- src/subcommands/actions.rs
+++ src/subcommands/actions.rs
@@ -3,9 +3,10 @@
 //!     - `list` - list all currently available actions
 
 // use repo::does_this_work;
+use colored::Colorize;
 use repo::errors::Result;
-use repo::Repo;
 use repo::{get_repos, GitGlobalConfig, GitGlobalResult};
+use repo::{Action, ActionError, Filterable, Repo, RepoTag};
 
 /// Forces the display of each repo path, without any extra output.
 pub fn list() -> Result<GitGlobalResult> {
@@ -19,6 +20,89 @@ pub fn list() -> Result<GitGlobalResult> {
     Ok(result)
 }
 
-pub fn perform() -> Result<GitGlobalResult> {
-    unimplemented!();
+pub fn perform(
+    tags: Option<String>,
+    path: Option<String>,
+    action: Option<String>,
+) -> Result<GitGlobalResult> {
+    let gc = GitGlobalConfig::new();
+    let mut repos = gc.get_cached_repos();
+    if let Some(tags) = tags {
+        let tags: Vec<RepoTag> = tags.split(",").map(RepoTag::new).collect();
+        repos = repos.filter_tags(tags);
+    }
+    if let Some(path) = path {
+        repos = repos.filter_paths(path);
+    }
+    for r in &repos {
+        println!("Doing {}", r.path);
+    }
+    // if gc.actions.
+    let action_name = action.unwrap();
+    match &gc
+        .actions
+        .iter()
+        .find(|a| a.name_match(&action_name).is_some())
+    {
+        Some(act) => {
+            if act.needs_path() {
+                println!("{}", "⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️".yellow());
+                for r in &repos {
+                    match act.path_bind(r.path.clone()) {
+                        Ok(a) => {
+                            let result = a.perform_action_for_repo();
+                            let ss = format!(
+                                "{} {} {} {}",
+                                "Result of action".blue(),
+                                a.get_name().green().underline(),
+                                "for".blue(),
+                                r.path().green().underline()
+                            );
+                            println!("{}", ss);
+                            for r in result.unwrap().split("\n") {
+                                println!("{}", r.green());
+                            }
+                            println!("{}", "**************".yellow());
+                        }
+                        Err(ActionError::NotANeedAPath(ae)) => {
+                            println!("Struck out: {}", ae);
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+                println!("{}", "⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️".yellow());
+            }
+            return Ok(GitGlobalResult::new(&repos));
+
+            // if let Ok(r) = act.perform_action_for_repo() {
+            //     println!("{}", r);
+            // }
+            // return Ok(GitGlobalResult::new(&repos));
+        }
+        None => {
+            println!("No actions matched {}", action_name);
+            gc.actions
+                .iter()
+                .for_each(|a| println!("Actual action: {}", a))
+        }
+    }
+
+    // for a in gc.actions {
+    //     // if let Some(act) = a.check_needs_path_match_name(&action_name, path) {
+    //     if let Some(act) = a.name_match(&action_name) {
+    //         let result = act.perform_action_for_repo();
+    //         // println!("RUN\n {}", result.unwrap());
+    //         println!("{}", result.unwrap().green());
+    //         // for r in result.unwrap().split("\n") {
+    //         //     println!("{}", r.green());
+    //         // }
+    //         return Ok(GitGlobalResult::new(&repos));
+    //     }
+    // }
+    println!("No actions matched {}", action_name);
+    // for a in gc.actions {
+    //     println!("Actual action: {}", a);
+    // }
+    Ok(GitGlobalResult::new(&repos))
+    // unimplemented!();
 }
