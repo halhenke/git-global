49e1bd34d5c6f098700339c2aaf7d2b5ad4fe3f4
Hal Henke <halhenke@gmail.com>
2020-05-17 16:35:36 +1000

    Try to implement something a bit more useful with `iced`

--- src/subcommands/prompt_iced.rs
+++ src/subcommands/prompt_iced.rs
@@ -6,6 +6,8 @@ use iced::{
     window::Settings as Window, Align, Application, Background, Button, Color,
     Column, Command, Container, Element, Length, Scrollable, Text,
 };
+use std::cell::{Ref, RefCell};
+use std::rc::Rc;
 
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
@@ -96,6 +98,106 @@ impl Application for Counter {
     }
 }
 
+#[derive(Default)]
+struct RepoList {
+    selected: i32,
+
+    // The local state of the two buttons
+    increment_button: button::State,
+    decrement_button: button::State,
+    list_of_things: scrollable::State,
+}
+
+impl Application for RepoList {
+    type Message = Message;
+    type Executor = executor::Null;
+    type Flags = ();
+
+    fn new(flags: ()) -> (Self, Command<Message>) {
+        (Self::default(), Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("A simple counter")
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::IncrementPressed => {
+                self.selected += 1;
+            }
+            Message::DecrementPressed => {
+                self.selected -= 1;
+            }
+        }
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        // Container::new()
+        // .push()
+        let b_bg: Background = Background::Color(Color {
+            r: 1.0,
+            g: 0.0,
+            b: 0.0,
+            a: 1.0,
+        });
+        let repos: Vec<String> = vec!["Haskell", "Swift", "Rust"].into_iter().map(|s| s.to_owned()).collect::<Vec<String>>();
+        let mut scroller : Scrollable<'_, Message> = Scrollable::new(&mut self.list_of_things);
+        // scroller = scroller.push(Text::new(repos.get(0).unwrap()));
+        // scroller.push(Text::new(repos.get(1).unwrap()));
+        for r in repos {
+            scroller = scroller.push(Text::new(r));
+        }
+        // let rc_scroll = Rc::new(scroller);
+        // let rc2 = Rc::clone(&rc_scroll);
+        // let rc3 = Rc::clone(&rc_scroll);
+        // repos.into_iter().for_each(|r| {
+        //     // (rc2).push(Text::new(r));
+        //     let s2 = scroller;
+        //     s2.push(Text::new(r));
+        //     scroller = s2;
+        //     // (scroller).push(Text::new(r));
+        // });
+        let col: Column<'_, Message> = Column::new()
+            .padding(100)
+            .spacing(40)
+            .width(Length::Fill)
+            // .max_width(500)
+            .align_items(Align::Center)
+            .push(
+                Button::new(&mut self.increment_button, Text::new("Scroll Up"))
+                    .on_press(Message::IncrementPressed),
+            )
+            .push(
+                Container::new(Text::new(self.selected.to_string()).size(50))
+                    .center_x(),
+            )
+            .push(
+                Button::new(
+                    &mut self.decrement_button,
+                    Text::new("Scroll Down"),
+                )
+                .on_press(Message::DecrementPressed),
+            )
+            .push(scroller);
+            // .push(rc3);
+        // .push(
+        //     Scrollable::new(&mut self.list_of_things)
+        //         .push(Text::new("A first thing"))
+        //         .push(Text::new("A second thing")),
+        // );
+        // .into();
+        let c = Container::new::<Element<Message>>(col.into())
+            // .max_width(1500)
+            // .max_height(100)
+            .width(Length::Units(400))
+            .center_x()
+            .center_y();
+        c.into()
+    }
+}
+
 pub fn go() -> Result<GitGlobalResult> {
     let mut gc = GitGlobalConfig::new();
     let w = Window {
@@ -109,8 +211,8 @@ pub fn go() -> Result<GitGlobalResult> {
         flags: (),
         window: w,
     };
-    Counter::run(s);
-    // Counter::run(Settings::default());
+    RepoList::run(s);
+    // Counter::run(s);
     let repos = gc.get_repos();
     let mut result = GitGlobalResult::new(&repos);
     return Ok(result);
